65f29d083af5a5bac369b8f48306e8d6d5922c36
AsliDasci <aslihandasci1@gmail.com>
2022-10-15 16:49:42 +0300

    Lesson1-Add Migration

---
+++ src/demoProjects/rentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories
+{
+    public interface IBrandRepository : IAsyncRepository<Brand>, IRepository<Brand>
+    {
+        //Brand'e özel operasyonlar yazılacak
+    }
+}
